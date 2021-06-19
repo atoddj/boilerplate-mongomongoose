e6059675ca991e7bfb2b2fec72f6bcca460a1982
Todd Jones <atoddj@gmail.com>
2021-06-19 10:58:51 -0400

    Fixed duplicate schema

--- myApp.js
+++ myApp.js
@@ -2,7 +2,15 @@ require('dotenv').config();
 var mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
-let Person;
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: {type: String, required: true},
+  age: {type: Number},
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
