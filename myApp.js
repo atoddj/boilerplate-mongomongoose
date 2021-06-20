3a83e80b926818dba6e3111e58c4a1c92db33a43
Todd Jones <atoddj@gmail.com>
2021-06-20 08:22:37 -0400

    updated edit handler

--- myApp.js
+++ myApp.js
@@ -40,7 +40,7 @@ const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
   Person.findById({_id: personId}, (err, docs) => {
     if(err) return console.error(err);
-    docs.favoriteFoods = [...favoriteFoods, foodToAdd];
+    docs.favoriteFoods.push(foodToAdd);
     docs.save((err,updatedPerson) => (err ? console.error(err) : done(null, updatedPerson)));
   });
 };
